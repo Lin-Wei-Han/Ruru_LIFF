31c1a0ef6f34d4d399d630aa424fb904c43d0e67
Lin-Wei-Han <judy97678@gmail.com>
2024-07-20 01:27:12 +0800

    test

---
+++ main.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const serviceCards = document.querySelectorAll('.service-card');
+  const orderAmount = document.querySelector('.order-amount');
+  const submitBtn = document.querySelector('.submit-btn');
+
+  serviceCards.forEach((card) => {
+    card.addEventListener('click', () => {
+      // 切換 active class 給點擊的 card
+      card.classList.toggle('active');
+
+      // 獲取對應的 dot 元素
+      const cardImgClass = card.querySelector('.card-img').classList[1];
+      const dot = document.querySelector(`.dot-${cardImgClass}`);
+
+      // 切換 dot 的顯示狀態
+      if (dot) {
+        if (card.classList.contains('active')) {
+          dot.style.display = 'block';
+        } else {
+          dot.style.display = 'none';
+        }
+      }
+
+      // 更新 order-amount
+      const activeCards = document.querySelectorAll('.service-card.active');
+      const activeCount = activeCards.length;
+      orderAmount.textContent = activeCount;
+
+      // 顯示或隱藏 submit-btn
+      submitBtn.style.display = activeCount > 0 ? 'block' : 'none';
+    });
+  });
+});
+
+// Create result table
+function createFlexMessage(selectedItems) {
+  const tableHtml = `
+        <table border="1" style="width:100%; border-collapse:collapse;">
+          <thead>
+            <tr>
+              <th>大小姐您的清單</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${selectedItems.map((item) => `<tr><td>${item}</td></tr>`).join('')}
+          </tbody>
+        </table>
+      `;
+  return tableHtml;
+}
+
+function callTutu() {
+  const activeCards = document.querySelectorAll('.service-card.active');
+  const selectedItems = Array.from(activeCards).map((card) => {
+    return card.querySelector('.card-title').textContent;
+  });
+
+  let message = createFlexMessage(selectedItems);
+  console.log(selectedItems);
+
+  liff
+    .sendMessages(message)
+    .then(() => {
+      liff.closeWindow();
+    })
+    .catch((err) => {
+      console.error(err.code, err.message);
+    });
+  // 在這裡處理選擇的項目列表
+}
